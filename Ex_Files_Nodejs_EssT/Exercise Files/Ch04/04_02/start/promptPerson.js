0ba1eb8385888df8255237769b44c913d299d8bc
Siddharth Mishra <siddharth82mishra@gmail.com>
2016-04-08 10:59:23 -0600

    Implemented readline object with Prompts.

---
+++ Ex_Files_Nodejs_EssT/Exercise Files/Ch04/04_02/start/promptPerson.js
@@ -0,0 +1,28 @@
+var readline = require('readline');
+var rl = readline.createInterface(process.stdin, process.stdout);
+
+var realPerson = { name: '', sayings: []};
+
+rl.question("What is your name?", function(answer){
+	realPerson.name = answer;
+
+	rl.setPrompt(`What would ${realPerson.name} say?`);
+	rl.prompt();
+	rl.on('line', function(saying){
+		if(saying.toLowerCase().trim() === 'exit'){
+			rl.close();
+		}
+		else{
+			realPerson.sayings.push(saying.trim());
+			rl.setPrompt(`What else would ${realPerson.name} say? `);
+			rl.prompt();
+			//global.console.log(`Saying: ${saying.trim()}.`);
+			//global.console.log(saying.trim());
+		}
+	});
+});
+
+rl.on('close', function(){
+	global.console.log("%s is a real person that says %j", realPerson.name, realPerson.sayings);
+	process.exit();
+})
