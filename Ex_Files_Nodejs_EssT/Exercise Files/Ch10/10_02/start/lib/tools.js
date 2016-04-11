4f0399284d39c07523807d21dbae86372a34f9fd
Siddharth Mishra <siddharth82mishra@gmail.com>
2016-04-11 10:32:24 -0600

    Async call back tested with mocha and chai

--- Ex_Files_Nodejs_EssT/Exercise Files/Ch10/10_02/start/lib/tools.js
+++ Ex_Files_Nodejs_EssT/Exercise Files/Ch10/10_02/start/lib/tools.js
@@ -1,7 +1,23 @@
+var https = require("https");
+
 module.exports = {
 
 	printName(person) {
 		return `${person.last}, ${person.first}`;
+	},
+
+	loadWiki(person, callback){
+		var url = `https://en.wikipedia.org/wiki/${person.first}_${person.last}`;
+		https.get(url, function(response){
+			var body = "";
+			response.on("data", function(chunk){
+				body += chunk;
+			});
+
+			response.on("end", function(){
+				callback(body);
+			})
+		});
 	}
 
 };
--- Ex_Files_Nodejs_EssT/Exercise Files/Ch10/10_02/start/test/tools-spec.js
+++ Ex_Files_Nodejs_EssT/Exercise Files/Ch10/10_02/start/test/tools-spec.js
@@ -1,14 +1,27 @@
 var expect = require("chai").expect;
 var tools = require("../lib/tools");
 
-describe("printName()", function() {
+describe("Tools", function(){
+	describe("printName()", function() {
 
-	it("should print the last name first", function() {
+		it("should print the last name first", function() {
 
-		var results = tools.printName({ first: "Alex", last: "Banks"});
+			var results = tools.printName({ first: "Alex", last: "Banks"});
 
-		expect(results).to.equal("Banks, Alex");
+			expect(results).to.equal("Banks, Alex");
 
+		});
 	});
 
+	// Load wiki will open wiki page.
+	describe("loadWiki()", function(){
+		this.timeout(5000);
+		it("Load Abraham Lincoln's wikipedia page.", function(done){
+			tools.loadWiki({first: "Abraham", last: "Lincoln"}, function(html){
+				expect(html).to.be.ok;
+				done();
+			})
+		});
+	});
 });
+
